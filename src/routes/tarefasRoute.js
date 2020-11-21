af72547cf65f95be2b5c5ab3264881cb020154c9
Dayanaferrer <dayanaferreira2@gmail.com>
2020-11-20 21:57:14 -0300

    routes

---
+++ src/routes/tarefasRoute.js
@@ -0,0 +1,14 @@
+require('dotenv-safe').config()
+
+const express = require("express")
+const router = express.Router()
+const controller = require("../controllers/tarefasController")
+
+router.get("/", controller.getAll)
+router.get("/:id", controller.getById)
+router.post("/", controller.postTarefa)
+router.delete("/:id", controller.delTarefa)
+router.delete("/", controller.delTarefaConcluida)
+router.put("/:id", controller.putTarefa)
+
+module.exports = router
